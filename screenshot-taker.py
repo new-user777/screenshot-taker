38d4d246675a6c170dce9656464be0f58b3357b2
Human <136667830+shopsaver@users.noreply.github.com>
2023-09-02 21:56:01 +0530

    Update screenshot-taker.py

--- screenshot-taker.py
+++ screenshot-taker.py
@@ -1,67 +1,85 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from PIL import Image
-import urllib.parse
 
 print("""
-Please Enter the followings
-***** Press 1 for a single domain *****
-***** Press 2 for multiple domains *****
+*****Press 1 for a single domain*****
+*****Press 2 for list of subdomain*****
+make sure you have entered a the path correctly
 """)
-option = int(input("[+] Enter the option: "))
-
-if option == 1:
-    subdomain = input("[+] Enter the subdomain: ")
+def capture_screenshots(option, subdomain=None, subdomain_list=None, output_path=None):
+    screenshot_filenames = []
     
-    options = Options()
-    options.add_argument('-headless')  # Run Firefox in headless mode
-    driver = webdriver.Firefox(options=options)
-    
-    try:
-        driver.get(subdomain)
-        
-        screenshot = driver.get_screenshot_as_png()
-        domain_name = subdomain.replace('http://', '').replace('https://', '').replace('.', '_')
-        screenshot_filename = f"{domain_name}.png"
-        
-        with open(screenshot_filename, "wb") as f:
-            f.write(screenshot)
-            
-        print(f"Screenshot saved for {subdomain}")
-    except Exception as e:
-        print(f"Error capturing screenshot for {subdomain}: {e}")
-    
-    # Close the browser
-    driver.quit()
+    if option == 1:
+        options = Options()
+        options.add_argument('-headless')  # Run Firefox in headless mode
+        driver = webdriver.Firefox(options=options)
 
-elif option == 2:
-    subdomain_list_path = input("[+] Enter the file path: ")
-    subdomains = open(subdomain_list_path, "r").read().splitlines()
-
-    options = Options()
-    options.add_argument('-headless')  # Run Firefox in headless mode
-    driver = webdriver.Firefox(options=options)
-
-    for sub in subdomains:
-        sub = sub.strip()  # Remove leading/trailing spaces if any
-        
         try:
-            driver.get(sub)
+            driver.get(subdomain)
             
             screenshot = driver.get_screenshot_as_png()
-            domain_name = sub.replace('http://', '').replace('https://', '').replace('.', '_')
+            domain_name = subdomain.replace('http://', '').replace('https://', '').replace('.', '_')
             screenshot_filename = f"{domain_name}.png"
+            
+            if output_path:
+                screenshot_filename = f"{output_path}/{screenshot_filename}"
             
             with open(screenshot_filename, "wb") as f:
                 f.write(screenshot)
-                
-            print(f"Screenshot saved for {sub}")
+            
+            screenshot_filenames.append(screenshot_filename)
+            print(f"Screenshot saved for {subdomain} at {screenshot_filename}")
         except Exception as e:
-            print(f"Error capturing screenshot for {sub}: {e}")
+            print(f"Error capturing screenshot for {subdomain}: {e}")
         
-    # Close the browser
-    driver.quit()
+        # Close the browser
+        driver.quit()
 
+    elif option == 2:
+        options = Options()
+        options.add_argument('-headless')  # Run Firefox in headless mode
+        driver = webdriver.Firefox(options=options)
+
+        for sub in subdomain_list:
+            sub = sub.strip()  # Remove leading/trailing spaces if any
+            
+            try:
+                driver.get(sub)
+                
+                screenshot = driver.get_screenshot_as_png()
+                domain_name = sub.replace('http://', '').replace('https://', '').replace('.', '_')
+                screenshot_filename = f"{domain_name}.png"
+                
+                if output_path:
+                    screenshot_filename = f"{output_path}/{screenshot_filename}"
+                
+                with open(screenshot_filename, "wb") as f:
+                    f.write(screenshot)
+                
+                screenshot_filenames.append(screenshot_filename)
+                print(f"Screenshot saved for {sub} at {screenshot_filename}")
+            except Exception as e:
+                print(f"Error capturing screenshot for {sub}: {e}")
+            
+        # Close the browser
+        driver.quit()
+
+    else:
+        print("Wrong Input")
+
+    return screenshot_filenames
+
+# Example usage:
+option = int(input("[+] Enter the option: "))
+if option == 1:
+    subdomain = input("[+] Enter the subdomain: ")
+    output_path = input("[+] Enter the path to save the screenshots: ")
+    capture_screenshots(option, subdomain=subdomain, output_path=output_path)
+elif option == 2:
+    subdomain_list_path = input("[+] Enter the file path: ")
+    subdomains = open(subdomain_list_path, "r").read().splitlines()
+    output_path = input("[+] Enter the path to save the screenshots: ")
+    capture_screenshots(option, subdomain_list=subdomains, output_path=output_path)
 else:
     print("Wrong Input")
 
